65c306a9e5c1e4cbf747d1ff3275c98d38fe5c43
James Downs <james.downs@nottingham.ac.uk>
2018-10-10 15:09:41 +0100

    Updated docstrings

--- preprocessing.py
+++ preprocessing.py
@@ -7,16 +7,66 @@ class VideoPreprocessor:
     """Class to manage the preprocessing of videos"""
 
     def __init__(self, input_video, output_video_filename):
+        """
+        Parameters
+        ----------
+        input_video: Class instance
+            An instance of the video.ReadVideo class containing a video
+
+        output_video_filename: string
+            The full filepath where the processed video will be saved
+        """
         self.vid = input_video
         self.output_video_filename = output_video_filename
 
     def _crop_and_mask_frame(self, frame, crop, mask_img):
+        """
+        Masks then crops a given frame
+
+        Takes a frame and uses a bitwise_and operation with the input mask_img
+        to mask the image around a shape.
+        It then crops the image around the mask.
+
+        Parameters
+        ----------
+        frame: numpy array
+            A numpy array of an image of type uint8
+
+        crop: a int list
+            A list in the format [ymin, ymax], [xmin, xmax] where x and y are
+            the points selected when you click during manual cropping
+
+        mask_img: numpy array
+            A 2D array with the same dimensions as frame which is used to mask
+            the image
+
+        Returns
+        -------
+        cropped_frame: numpy array
+            A numpy array containing an image which has been cropped and masked
+        """
         masked_frame = cv2.bitwise_and(frame, frame, mask=mask_img)
-        cropped_frame = masked_frame[crop[0][0]:crop[0][1], crop[1][0]:crop[1][1], :]
+        cropped_frame = masked_frame[crop[0][0]:crop[0][1],
+                                     crop[1][0]:crop[1][1],
+                                     :]
         return cropped_frame
 
     def crop_and_mask(self, no_of_sides=1, show=False):
-        """Crop a video to a mask"""
+        """
+        Crops a video to a mask and saves it to file
+
+        Allows the user to manually select a region which the method will send
+        to the _crop_and_mask method to be masked and cropped.
+
+        Parameters
+        ----------
+        no_of_sides: int
+            The number of sides that the desired shape contains.
+                1: Uses a circle
+                >2: Uses a polygon
+        show: Bool
+            If true displays the first cropped and masked frame
+        """
         frame = self.vid.read_next_frame()
         crop_inst = CropShape(frame, no_of_sides)
         mask_img, crop = crop_inst.begin_crop()
@@ -25,7 +75,8 @@ class VideoPreprocessor:
             cv2.imshow("cropped frame", cropped_frame)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
-        self.output_video = video.WriteVideo(self.output_video_filename, frame_size=np.shape(cropped_frame))
+        self.output_video = video.WriteVideo(self.output_video_filename,
+                                             frame_size=np.shape(cropped_frame))
         self.output_video.add_frame(cropped_frame)
         for f in np.arange(1, self.vid.num_frames):
             frame = self.vid.read_next_frame()
@@ -41,9 +92,14 @@ class CropShape:
         """
         Initialise with input image and the number of sides:
 
-        Inputs:
-            --> input_image = a 2D numpy array containing an image
-            --> no_of_sides = an integer. If 1, draws circle. If 3 or more, draws polygon.
+        Parameters
+        ----------
+        input_image: 2D numpy array
+            contains an image
+        no_of_sides: int
+            The number of sides that the desired shape contains.
+                1: Uses a circle
+                >2: Uses a polygon
 
         """
         super().__init__()
@@ -53,7 +109,7 @@ class CropShape:
         self.no_of_sides = no_of_sides
 
     def _click_and_crop(self, event, x, y, flags, param):
-        """Method to manage the user cropping"""
+        """Internal method to manage the user cropping"""
         if event == cv2.EVENT_LBUTTONDOWN:
             self.refPt = [(x, y)]
             self.cropping = True
@@ -62,9 +118,11 @@ class CropShape:
             self.cropping = False
             if self.no_of_sides == 1:
                 self.refPt.append((x, y))
-                cx = (self.refPt[1][0] - self.refPt[0][0])/2 + self.refPt[0][0]
-                cy = (self.refPt[1][1] - self.refPt[0][1])/2 + self.refPt[0][1]
-                rad = int((self.refPt[1][0] - self.refPt[0][0])/2)
+                cx = ((self.refPt[1][0] - self.refPt[0][0]) / 2 +
+                      self.refPt[0][0])
+                cy = ((self.refPt[1][1] - self.refPt[0][1]) / 2 +
+                      self.refPt[0][1])
+                rad = int((self.refPt[1][0] - self.refPt[0][0]) / 2)
                 cv2.circle(self.image, (int(cx), int(cy)), rad, (0, 255, 0), 2)
                 cv2.imshow("image", self.image)
 
@@ -98,22 +156,28 @@ class CropShape:
         cv2.destroyAllWindows()
 
         if self.no_of_sides == 1:
-            cx = (self.refPt[1][0] - self.refPt[0][0])/2 + self.refPt[0][0]
-            cy = (self.refPt[1][1] - self.refPt[0][1])/2 + self.refPt[0][1]
-            rad = int((self.refPt[1][0] - self.refPt[0][0])/2)
-            roi = clone[int(cy-rad):int(cy+rad), self.refPt[0][0]:self.refPt[1][0]]
+            cx = (self.refPt[1][0] - self.refPt[0][0]) / 2 + self.refPt[0][0]
+            cy = (self.refPt[1][1] - self.refPt[0][1]) / 2 + self.refPt[0][1]
+            rad = int((self.refPt[1][0] - self.refPt[0][0]) / 2)
+            roi = clone[int(cy - rad):int(cy + rad),
+                        self.refPt[0][0]:self.refPt[1][0]]
             mask_img = np.zeros((np.shape(clone))).astype(np.uint8)
-            cv2.circle(mask_img, (int(cx), int(cy)), rad, [1, 1, 1], thickness=-1)
-            crop = [int(cy - rad), int(cy + rad)], [self.refPt[0][0], self.refPt[1][0]]
+            cv2.circle(mask_img, (int(cx), int(cy)), rad, [1, 1, 1],
+                       thickness=-1)
+            crop = ([int(cy - rad), int(cy + rad)],
+                    [self.refPt[0][0], self.refPt[1][0]])
 
         else:
             roi = clone.copy()
-            for n in np.arange(-1, len(points)-1):
+            for n in np.arange(-1, len(points) - 1):
                 roi = cv2.line(roi, (int(points[n, 0]), int(points[n, 1])),
-                               (int(points[n+1, 0]), int(points[n+1, 1])), [0, 255, 0], 2)
+                               (int(points[n + 1, 0]), int(points[n + 1, 1])),
+                               [0, 255, 0], 2)
             mask_img = np.zeros(np.shape(clone)).astype('uint8')
-            cv2.fillPoly(mask_img, pts=np.array([points], dtype=np.int32), color=(250, 250, 250))
-            crop = [min(points[:, 1]), max(points[:, 1])], [min(points[:, 0]), max(points[:, 0])]
+            cv2.fillPoly(mask_img, pts=np.array([points], dtype=np.int32),
+                         color=(250, 250, 250))
+            crop = ([min(points[:, 1]), max(points[:, 1])],
+                    [min(points[:, 0]), max(points[:, 0])])
         cv2.imshow("ROI", roi)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
@@ -121,7 +185,8 @@ class CropShape:
 
 
 if __name__ == "__main__":
-    vid = video.ReadVideo("/home/ppxjd3/Code/ParticleTracking/test_data/test_video_EDIT.avi")
+    vid = video.ReadVideo(
+        "/home/ppxjd3/Code/ParticleTracking/test_data/test_video_EDIT.avi")
     output_video_filename = "/home/ppxjd3/Code/ParticleTracking/test_data/test_video_crop.avi"
     VP = VideoPreprocessor(vid, output_video_filename)
     VP.crop_and_mask(no_of_sides=4, show=True)
