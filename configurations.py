cd8a2b4fe8ddbca49f47996160db727db325a1fc
James <jamesdownslab@gmail.com>
2019-03-29 15:15:56 +0000

    Config file to hold options for different systems

---
+++ configurations.py
@@ -0,0 +1,13 @@
+"""Add methods and parameters for different systems here"""
+NITRILE_BEADS_METHOD = ['flip']
+NITRILE_BEADS_PARAMETERS = {
+    'number of tray sides': 6,
+    'min_dist': 23,
+    'p_1': 105,
+    'p_2': 2,
+    'min_rad': 13,
+    'max_rad': 14,
+    'max frame displacement': 10,
+    'min frame life': 50,
+    'memory': 3
+    }
