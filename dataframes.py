738d1ffc32a37fc4ec14c2e973fa49ccfcd7de5d
James Downs <jamesdownslab@gmail.com>
2019-04-30 12:00:33 +0100

    Reformatting

--- dataframes.py
+++ dataframes.py
@@ -1,8 +1,9 @@
-import pandas as pd
-import numpy as np
 import os
-import itertools
 import warnings
+
+import numpy as np
+import pandas as pd
+
 warnings.filterwarnings('ignore', category=pd.io.pytables.PerformanceWarning)
 
 
