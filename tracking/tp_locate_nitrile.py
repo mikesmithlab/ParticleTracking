cf88cc23f9d162cd00028fcdbe1ba484b6d8962b
James <jamesdownslab@gmail.com>
2019-06-05 13:40:27 +0100

    Added comments to filter

--- tracking/tp_locate_nitrile.py
+++ tracking/tp_locate_nitrile.py
@@ -56,14 +56,21 @@ def read_audio_file(file, frames):
 
 def filter_near_edge(feat, boundary, threshold):
     print('filtering at edge')
+    # Create line equations of form ax + by + c = 0 from corners
     line_ends = [[boundary[i-1, :], boundary[i, :]] for i in range(6)]
     a, b, c = zip(
         *[[(yb - ya) / (xa - xb), 1, xa * (ya - yb) / (xa - xb) - ya] for
           (xa, ya), (xb, yb) in line_ends])
     a, b, c = np.array(a, ndmin=2), np.array(b, ndmin=2), np.array(c, ndmin=2)
+
+    # Get points
     points = feat[['x', 'y']].values
     x, y = points[:, 0, np.newaxis], points[:, 1, np.newaxis]
+
+    # Find distance from each point to each line
     d = np.abs(x@a + y@b + c) / (np.sqrt(a**2 + b**2))
+
+    # Find distance to closest line for each point
     d = np.min(d, axis=1)
     return feat[d > threshold]
 
