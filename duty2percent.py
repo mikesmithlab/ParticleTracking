917e35e7fd6615a93ad68e7b0c7e2a0da640b642
James <jamesdownslab@gmail.com>
2019-06-28 15:40:43 +0100

    Changed path to relative

--- duty2percent.py
+++ duty2percent.py
@@ -3,5 +3,5 @@ import numpy as np
 
 def convert(duty):
     duty = duty.astype(np.int32)
-    percent = np.loadtxt('duty2percent.txt')
+    percent = np.loadtxt('ParticleTracking/duty2percent.txt')
     return percent[duty]
