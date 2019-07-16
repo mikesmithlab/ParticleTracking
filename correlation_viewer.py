a15f961c3636e621d363bc1527e44316129caae8
James <jamesdownslab@gmail.com>
2019-07-16 17:09:46 +0100

    Added QtWidgets to Generic

--- correlation_viewer.py
+++ correlation_viewer.py
@@ -1,12 +1,10 @@
-from ParticleTracking import correlations
-from PyQt5.QtWidgets import (QMainWindow, QApplication, QHBoxLayout, QWidget,
-                             QVBoxLayout, QSlider, QSizePolicy, QFileDialog,
-                             QLabel, QComboBox)
-from matplotlib.backend_bases import key_press_handler
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, \
-    NavigationToolbar2QT
-from matplotlib.figure import Figure
+import sys
 
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QMainWindow, QApplication, QHBoxLayout, QWidget,
+                             QVBoxLayout, QFileDialog)
+
+from Generic import pyqt5_widgets
 from ParticleTracking import correlations
 
 
@@ -27,8 +25,11 @@ class MainWindow(QMainWindow):
     def setup_main_widget(self):
         self.main_widget = QWidget(self)
         vbox = QVBoxLayout(self.main_widget)
-
-        vbox.addLayout(self.setup_slider_box())
+        duty_slider = pyqt5_widgets.Slider(
+            self.main_widget, 'Duty', self.duty_changed, 0,
+            len(self.data.duty) - 1, 1, 0)
+        # vbox.addLayout(self.setup_slider_box())
+        vbox.addWidget(duty_slider)
         hbox = QHBoxLayout()
         g_box = self.create_g_box()
         g6_box = self.create_g6_box()
@@ -37,146 +38,169 @@ class MainWindow(QMainWindow):
         vbox.addLayout(hbox)
         self.setCentralWidget(self.main_widget)
 
-    def setup_slider_box(self):
-        label = QLabel(self.main_widget)
-        label.setText('Duty')
-
-        hbox = QHBoxLayout()
-        duty_slider = QSlider(Qt.Horizontal)
-        duty_slider.setRange(0, len(self.data.duty))
-
-        duty_label = QLabel(self.main_widget)
-        duty_label.setText(str(self.data.duty[0]))
-
-        def slider_changed(value):
-            duty_label.setText(str(self.data.duty[value]))
-            self.change_duty(value)
-
-        duty_slider.valueChanged[int].connect(slider_changed)
-        hbox.addWidget(label)
-        hbox.addWidget(duty_slider)
-        hbox.addWidget(duty_label)
-        return hbox
+    def duty_changed(self, value):
+        duty = self.data.duty[int(value)]
+        # self.change_duty(duty)
+        r, g, g6 = self.data.get(duty)
+        self.g_graph.set_data(r, g, g6)
+        self.g6_graph.set_data(r, g, g6)
 
     def create_g_box(self):
         self.g_graph = GGraph(self.main_widget)
 
-        height_slider = hbox_slider(
-            self.main_widget, 'line height', -1, 1, 10, 0,
-            self.g_graph.change_line_height)
+        height_slider = pyqt5_widgets.Slider(
+            self.main_widget, 'line height', self.g_graph.set_offset,
+            -1, 1, 100, 0)
 
-        projection_combo = combo_box(
+        projection_combo = pyqt5_widgets.ComboBox(
             self.main_widget, 'projection',
             ['linear', 'logx', 'logy', 'loglog'],
             self.g_graph.change_projection)
 
+        autoscale_checkbox = pyqt5_widgets.CheckBox(
+            self.main_widget,
+            'autoscale',
+            self.g_graph.set_autoscale,
+            'on')
+
+        peak_finder = PeakFinder(self.main_widget, self.g_graph)
+
         vbox = QVBoxLayout()
         vbox.addWidget(self.g_graph)
-        vbox.addLayout(height_slider)
-        vbox.addLayout(projection_combo)
+        vbox.addWidget(height_slider)
+        vbox.addWidget(projection_combo)
+        vbox.addWidget(autoscale_checkbox)
+        vbox.addWidget(peak_finder)
         return vbox
 
     def create_g6_box(self):
-        vbox = QVBoxLayout()
         self.g6_graph = G6Graph(self.main_widget)
-        height_slider = hbox_slider(
-            self.main_widget, 'line height', -1, 1, 10, 0,
-            self.g6_graph.change_line_height)
-        projection_combo = combo_box(
+
+        height_slider = pyqt5_widgets.Slider(
+            self.main_widget, 'line height', self.g6_graph.set_offset,
+            -1, 1, 100, 0)
+
+        projection_combo = pyqt5_widgets.ComboBox(
             self.main_widget, 'projection',
             ['linear', 'logx', 'logy', 'loglog'],
             self.g6_graph.change_projection)
-        vbox.addWidget(self.g6_graph)
-        vbox.addLayout(height_slider)
-        vbox.addLayout(projection_combo)
-        return vbox
 
-    def change_duty(self, value):
-        r, g, g6 = self.data.get(self.data.duty[value])
-        self.g_graph.set_data(r, g, g6)
-        self.g6_graph.set_data(r, g, g6)
+        autoscale_checkbox = pyqt5_widgets.CheckBox(
+            self.main_widget,
+            'autoscale',
+            self.g6_graph.set_autoscale,
+            'on')
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.g6_graph)
+        vbox.addWidget(height_slider)
+        vbox.addWidget(projection_combo)
+        vbox.addWidget(autoscale_checkbox)
+        return vbox
 
     def initial_plot(self):
         r, g, g6 = self.data.get(self.data.duty[0])
-        self.g_graph.plot(r, g, g6)
-        self.g6_graph.plot(r, g, g6)
+        self.g_graph.set_data(r, g, g6)
+        self.g6_graph.set_data(r, g, g6)
 
 
-def combo_box(parent, label, items, function):
-    lbl = QLabel(label, parent)
-    combo = QComboBox(parent)
-    combo.addItems(items)
-    combo.activated[str].connect(function)
+class PeakFinder(QWidget):
+    def __init__(self, parent, graph):
+        self.show_fit = False
+        self.show_peaks = False
+        QWidget.__init__(self, parent)
+        self.setLayout(QVBoxLayout())
+        self.create_widgets()
 
-    hbox = QHBoxLayout()
-    hbox.addWidget(lbl)
-    hbox.addWidget(combo)
-    hbox.addStretch(1)
-    return hbox
+    def create_widgets(self):
+        # checkboxes
+        show_peaks = pyqt5_widgets.CheckBox(
+            self,
+            'show peaks',
+            self.show_peaks_changed)
+        show_fit = pyqt5_widgets.CheckBox(
+            self,
+            'show fit',
+            self.show_fit_changed)
+        hbox = QHBoxLayout()
+        hbox.addWidget(show_peaks)
+        hbox.addWidget(show_fit)
+        self.layout().addLayout(hbox)
+
+        # sliders
+        height_slider = pyqt5_widgets.CheckedSlider(
+            self, 'height', self.height_changed, 0, 50, 1, 0)
+        threshold_slider = pyqt5_widgets.CheckedSlider(
+            self, 'threshold', self.threshold_changed, 0, 1, 10, 0)
+
+        self.layout().addWidget(height_slider)
+        self.layout().addWidget(threshold_slider)
+
+    def show_peaks_changed(self, state):
+        self.show_peaks = True if state == Qt.Checked else False
+
+    def show_fit_changed(self, state):
+        self.show_fit = True if state == Qt.Checked else False
+
+    def height_changed(self, value):
+        self.height = value
+
+    def threshold_changed(self, value):
+        self.threshold = value
 
 
-def hbox_slider(parent, label, start, end, dpi, initial, function):
-    start *= dpi
-    end *= dpi
-    initial *= dpi
-
-    hbox = QHBoxLayout()
-    lab = QLabel(parent)
-    lab.setText(label)
-    slider = QSlider(Qt.Horizontal)
-    slider.setRange(start, end)
-    slider.setSliderPosition(initial)
-
-    val_label = QLabel(parent)
-    val_label.setText(str(initial / dpi))
-
-    def slider_changed(value):
-        val_label.setText(str(value / dpi))
-        function(value / dpi)
-
-    slider.valueChanged[int].connect(slider_changed)
-    hbox.addWidget(lab)
-    hbox.addWidget(slider)
-    hbox.addWidget(val_label)
-    return hbox
-
-
-class Graph(FigureCanvasQTAgg):
-
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        self.type = type
-        self.fig = Figure()
-
-        FigureCanvasQTAgg.__init__(self, self.fig)
-        self.setParent(parent)
-        FigureCanvasQTAgg.setSizePolicy(self,
-                                        QSizePolicy.Expanding,
-                                        QSizePolicy.Expanding)
-        self.canvas = FigureCanvasQTAgg(self.fig)
-        self.canvas.setFocusPolicy(Qt.StrongFocus)
-        self.canvas.mpl_connect('key_press_event', self.on_key_press)
-        self.toolbar = NavigationToolbar2QT(self.canvas, self)
-
+class Graph(pyqt5_widgets.MatplotlibFigure):
+    def __init__(self, parent, power):
+        self.power = power
+        pyqt5_widgets.MatplotlibFigure.__init__(self, parent)
         self.setup_axes()
-
-    def on_key_press(self, event):
-        key_press_handler(event, self.canvas, self.toolbar)
+        self.initial_plot()
+        self.setup_variables()
 
     def setup_axes(self):
         self.ax = self.fig.add_subplot(111)
 
+    def initial_plot(self):
+        self.line, = self.ax.plot([], [])
+        self.power_line, = self.ax.plot([], [])
+        self.draw()
+
+    def setup_variables(self):
+        self.offset = 0
+        self.autoscale = True
+        self.peaks_on = False
+
     def set_labels(self, xlabel, ylabel):
         self.ax.set_xlabel(xlabel)
         self.ax.set_ylabel(ylabel)
+        self.fig.tight_layout()
 
-    def change_projection(self, key):
-        if key == 'linear':
+    def update(self):
+        self.line.set_xdata(self.x)
+        self.line.set_ydata(self.y)
+        if self.autoscale:
+            self.ax.relim()
+            self.ax.autoscale_view()
+        self.draw()
+        self.update_power_line()
+
+    def update_power_line(self):
+        self.power_line.set_xdata(self.x)
+        self.power_line.set_ydata(self.x ** self.power + self.offset)
+        self.draw()
+
+    def set_offset(self, offset):
+        self.offset = offset
+        self.update_power_line()
+
+    def change_projection(self, choice):
+        if choice == 'linear':
             self.ax.set_xscale('linear')
             self.ax.set_yscale('linear')
-        elif key == 'logx':
+        elif choice == 'logx':
             self.ax.set_xscale('log')
             self.ax.set_yscale('linear')
-        elif key == 'logy':
+        elif choice == 'logy':
             self.ax.set_xscale('linear')
             self.ax.set_yscale('log')
         else:
@@ -184,48 +208,33 @@ class Graph(FigureCanvasQTAgg):
             self.ax.set_yscale('log')
         self.draw()
 
+    def set_autoscale(self, state):
+        if state == Qt.Checked:
+            self.autoscale = True
+        else:
+            self.autoscale = False
+
 
 class GGraph(Graph):
     def __init__(self, parent=None):
-        super().__init__(parent)
+        super().__init__(parent, -1 / 3)
         self.set_labels('r', '$G(r)$')
 
-    def plot(self, r, g, g6):
-        self.r = r
-        choice = {'G': g - 1, 'G6': g6 / g}
-        self.line, = self.ax.plot(r, g - 1)
-        self.power_line, = self.ax.plot(r, r ** (-1 / 3))
-
     def set_data(self, r, g, g6):
-        self.r = r
-        self.line.set_xdata(r)
-        self.line.set_ydata(g - 1)
-        self.draw()
-
-    def change_line_height(self, value):
-        self.power_line.set_ydata(self.r ** (-1 / 3) + value)
-        self.draw()
+        self.x = r
+        self.y = g - 1
+        self.update()
 
 
 class G6Graph(Graph):
     def __init__(self, parent=None):
-        super().__init__(parent)
+        super().__init__(parent, -1 / 4)
         self.set_labels('r', '$G_6(r)$')
 
-    def plot(self, r, g, g6):
-        self.r = r
-        self.line, = self.ax.plot(r, g6 / g)
-        self.power_line, = self.ax.plot(r, r ** (-1 / 4))
-
     def set_data(self, r, g, g6):
-        self.r = r
-        self.line.set_xdata(r)
-        self.line.set_ydata(g6 / g)
-        self.draw()
-
-    def change_line_height(self, value):
-        self.power_line.set_ydata(self.r ** (-1 / 4) + value)
-        self.draw()
+        self.x = r
+        self.y = g6 / g
+        self.update()
 
 
 class Data:
@@ -242,7 +251,6 @@ class Data:
         data = self.df.loc[self.df.d == d, ['r', 'g', 'g6']].values
         r, g, g6 = data.T
         return r[0], g[0], g6[0]
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
