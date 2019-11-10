bcd53879a622af337705069c242f27a0e17f296d
mike <mike.i.smith@nottingham.ac.uk>
2019-11-10 21:39:46 +0000

    Added help info to preprocessing_methods

--- preprocessing/preprocessing_methods.py
+++ preprocessing/preprocessing_methods.py
@@ -4,12 +4,38 @@ import cv2
 import numpy as np
 from ParticleTracking.general.parameters import  get_param_val
 
-'''Should ideally be in tracking_methods'''
-#def distance(frame, parameters):
-#    dist = cv2.distanceTransform(frame, cv2.DIST_L2, 5)
-#    return dist
+
+def distance(frame, parameters=None):
+    '''
+    Performs distance transform:
+
+    distance implements the opencv distance transform. This
+    transform operates on a binary image. For each chosen white pixel
+    it calculates the distance to the nearest black pixel. This
+    distance is the value of the chosen pixel. Thus if operating on
+    a white circle the distance transform is a maximum at the middle and
+    1 at the perimeter.
+
+    :param frame: binary frame
+    :param parameters: parameters dictionary
+
+    :return: The distance transform image.
+    '''
+    dist = cv2.distanceTransform(frame, cv2.DIST_L2, 5)
+    return dist
 
 def grayscale(frame, parameters=None):
+    '''
+    Convert colour to grayscale:
+
+    This function converts a bgr colour image into a monochrome
+    or grayscale image.
+
+    :param frame: A 3 channel colour image
+    :param parameters: parameters dictionary
+
+    :return: A grayscale image
+    '''
     return images.bgr_2_grayscale(frame)
 
 
@@ -39,14 +65,22 @@ def crop_and_mask(frame, parameters=None):
 
 def subtract_bkg(frame, parameters=None):
     '''
-        Send grayscale frame. Finds mean value of background and then returns
-        frame which is the absolute difference of each pixel from that value
-        normalise=True will set the largest difference to 255
+    Subtract bkg image
 
-        :param frame:
-        :return:
-        '''
-    norm = parameters['subtract bkg norm']
+    This function subtracts a background image from a grayscale frame.
+
+    options:
+    parameters['subtract bkg type'] == 'mean' : subtracts the mean intensity from image
+                                    == 'img' : subtracts a pre-prepared background image.
+                                            (See preprocessing > meanbkg_img.py)
+    parameters['subtract bkg norm'] == True   : Stretches range of final image intensities 0-255
+
+    :param frame: grayscale image
+    :param parameters: parameters dictionary
+
+    :return: image with background image subtracted
+    '''
+
 
     if parameters['subtract bkg type'] == 'mean':
         mean_val = int(np.mean(frame))
@@ -61,7 +95,7 @@ def subtract_bkg(frame, parameters=None):
 
     frame = cv2.subtract(subtract_frame, frame)
 
-    if norm == True:
+    if parameters['subtract bkg norm'] == True:
         frame = cv2.normalize(frame, None, alpha=0, beta=255,
                               norm_type=cv2.NORM_MINMAX)
 
@@ -70,14 +104,23 @@ def subtract_bkg(frame, parameters=None):
 
 def variance(frame, parameters=None):
     '''
-    Send grayscale frame. Finds mean value of background and then returns
-    frame which is the absolute difference of each pixel from that value
-    normalise=True will set the largest difference to 255
+    variance image
 
-    :param frame:
-    :return:
+    This function finds the mean value of image and then returns
+    frame which is the absolute difference of each pixel from that value
+
+    options:
+    parameters['variance type'] == 'mean' : Returns the absolute difference from the mean
+                                            img value
+    parameters['variance type'] == 'img'  : Returns the absolute difference from a supplied
+                                            bkg img. Bkg img is stored in parameters['bkg_img'].
+    parameters['variance bkg norm'] == True: will stretch the range of the largest difference to 255
+
+    :param frame: grayscale image
+    :param parameters: parameters dictionary
+
+    :return: image of absolute difference from mean
     '''
-    norm=parameters['variance bkg norm']
 
     if parameters['variance type'] == 'mean':
         mean_val = int(np.mean(frame))
@@ -85,33 +128,75 @@ def variance(frame, parameters=None):
     elif parameters['variance type'] == 'img':
         temp_params = {}
         temp_params['blur kernel'] = get_param_val(parameters['variance blur kernel'].copy())
-        #This option subtracts the previously created image which is added to dictionary.
         subtract_frame = parameters['bkg_img']
         frame = blur(frame, temp_params)
         subtract_frame = blur(subtract_frame, temp_params)
     elif parameters['variance type'] == 'zeros':
         subtract_frame = np.zeros(np.shape(frame))
+
     frame1 = cv2.subtract(subtract_frame, frame)
     frame1 = cv2.normalize(frame1, frame1 ,0,255,cv2.NORM_MINMAX)
     frame2 = cv2.subtract(frame, subtract_frame)
     frame2 = cv2.normalize(frame2, frame2,0,255,cv2.NORM_MINMAX)
     frame = cv2.add(frame1, frame2)
-    if norm == True:
+
+    if parameters['variance bkg norm'] == True:
         frame = cv2.normalize(frame, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
 
     return frame
 
 def flip(frame, parameters=None):
+    '''
+    Inverts a binary frame
+
+    :param frame: binary frame
+    :param parameters: parameters dictionary
+
+    :return: inverted binary frame
+    '''
     return ~frame
 
 
 def threshold(frame, parameters=None):
+    '''
+    Apply a global image threshold
+
+    This takes a cutoff threshold value and returns white above and
+    black below this value.
+
+    option:
+    parameters['threshold'] : sets the value of the cutoff threshold
+    parameters['threshold mode] : Can be used to invert the above behaviour
+
+    :param frame: grayscale img
+    :param parameters: parameters dictionary
+
+    :return: binary image
+    '''
     threshold = get_param_val(parameters['threshold'])
     mode = get_param_val(parameters['threshold mode'])
     return images.threshold(frame, threshold, mode)
 
 
 def adaptive_threshold(frame, parameters=None):
+    '''
+    Adaptive threshold
+
+    This applies an adaptive threshold. This differs from global threshold
+    in that for each pixel the cutoff threshold is defined based on a block of local
+    pixels around it. This enables you to cope with gradual changes in illumination
+    across the image etc.
+
+    options:
+    parameters['adaptive threshold block size'] : Size of local block of pixels to calculate threshold on
+    parameters['adaptive threshold C'] : The mean-c value see here: http://homepages.inf.ed.ac.uk/rbf/HIPR2/adpthrsh.htm
+    parameters['adaptive threshold mode'] : inverts behaviour
+
+    :param frame: grayscale image
+    :param parameters: parameters dictionary
+
+    :return: binary image
+    '''
     block = get_param_val(parameters['adaptive threshold block size'])
     const = get_param_val(parameters['adaptive threshold C'])
     invert = get_param_val(parameters['adaptive threshold mode'])
@@ -122,34 +207,132 @@ def adaptive_threshold(frame, parameters=None):
 
 
 def blur(frame, parameters=None):
+    '''
+    Gaussian blur
+
+    Applies a gaussian blur to the image (https://en.wikipedia.org/wiki/Gaussian_blur)
+    Usually useful to apply before subtracting 2 images.
+
+    options:
+    parameters['blur kernel'] specifies the dimensions of a square kernel
+
+    :param frame: grayscale image
+    :param parameters: parameters dictionary
+
+    :return: blurred image
+    '''
     kernel = get_param_val(parameters['blur kernel'])
     return images.gaussian_blur(frame, (kernel, kernel))
 
 def medianblur(frame, parameters=None):
+    '''
+    Median blur
+
+    Applies a median blur to the image (https://en.wikipedia.org/wiki/Median_filter)
+    Good for removing speckle noise
+
+    options:
+    parameters['blur kernel'] specifies the dimensions of a square kernel
+
+    :param frame: grayscale image
+    :param parameters: parameters dictionary
+
+    :return: blurred image
+    '''
     kernel = get_param_val(parameters['blur kernel'])
     return images.median_blur(frame, kernel)
 
 def opening(frame, parameters=None):
+    '''
+    Opening an image
+
+    This is a morphological operation which applies an erosion followed by a dilation
+    This tends to remove small isolated objects.
+    https://homepages.inf.ed.ac.uk/rbf/HIPR2/open.htm
+
+    options:
+    parameters['opening kernel'] specifies the dimensions of a square kernel
+
+    :param frame: binary image
+    :param parameters: parameters dictionary
+
+    :return: binary image
+    '''
     kernel = get_param_val(parameters['opening kernel'])
     return images.opening(frame, (kernel, kernel))
 
 
 def closing(frame, parameters=None):
+    '''
+    Closing an image
+
+    This is a morphological operation which applies a dilation followed
+    by an erosion. Tends to fill in holes.
+    https://homepages.inf.ed.ac.uk/rbf/HIPR2/close.htm
+
+    options:
+    parameters['closing kernel'] specifies the dimensions of a square kernel
+
+    :param frame: binary image
+    :param parameters: parameters dictionary
+
+    :return: binary image
+    '''
     kernel = get_param_val(parameters['closing kernel'])
     return images.closing(frame, (kernel, kernel))
 
 
 def dilate(frame, parameters=None):
+    '''
+    dilation of an image
+
+    This is a morphological operation which applies a kernel to an image to add
+    to edge pixels. https://homepages.inf.ed.ac.uk/rbf/HIPR2/dilate.htm
+
+    options:
+    parameters['dilate kernel'] specifies the dimensions of a square kernel
+
+    :param frame: binary image
+    :param parameters: parameters dictionary
+
+    :return: binary image
+    '''
     kernel = get_param_val(parameters['dilate kernel'])
     return images.dilate(frame, (kernel, kernel))
 
 
 def erode(frame, parameters=None):
+    '''
+    dilation of an image
+
+    This is a morphological operation which applies a kernel to an image to remove
+    edge pixels. https://homepages.inf.ed.ac.uk/rbf/HIPR2/erode.htm
+
+    options:
+    parameters['erode kernel'] specifies the dimensions of a square kernel
+
+    :param frame: binary image
+    :param parameters: parameters dictionary
+
+    :return: binary image
+    '''
     kernel = get_param_val(parameters['erode kernel'])
     return images.erode(frame, (kernel, kernel))
 
 
 def adjust_gamma(image, parameters=None):
+    '''
+    Gamma correction
+
+    generates a lookup table which maps the values 0-255 to 0-255
+    however not in a linear way. The mapping follows a power law
+    with exponent gamma.
+
+    :param image: grayscale image
+    :param parameters: parameters dictionary
+
+    :return: image
+    '''
     gamma = get_param_val(parameters['gamma'])/100.0
     # build a lookup table mapping the pixel values [0, 255] to
     # their adjusted gamma values
@@ -161,6 +344,19 @@ def adjust_gamma(image, parameters=None):
     return cv2.LUT(image, table)
 
 def resize(frame, parameters=None):
+    '''
+    Resize an image
+
+    resizes an input image by the scale specified
+
+    options:
+    parameters['resize scale'] : factor for scale operation
+
+    :param frame: colour, grayscale or binary image
+    :param parameters: parameters dictionary
+
+    :return: image
+    '''
     scale = get_param_val(parameters['resize scale'])
     return images.resize(frame, scale)
 
